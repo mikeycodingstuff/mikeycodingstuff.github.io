42566042facdab6455ca49de913c5c6d76422401
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-06-29 19:49:27 +0100

    testing dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import Welcome from '@/components/Welcome.vue'
-import { ref } from 'vue'
+import Welcome from '@/components/Welcome.vue';
+import { useDark, useToggle } from '@vueuse/core';
 
-let count = ref(0);
-
-function increment() {
-  count.value++
-}
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <header class="flex flex-col justify-center items-center">
-    <Welcome msg="Hello! Testing!" class="mb-5"/>
-    <div class="flex items-center justify-between w-full gap-5">
-      <div class="text-2xl">count: {{ count }}</div>
-      <button @click="increment" class="border border-black rounded p-2">+</button>
-    </div>
+    <Welcome msg="Hello! Testing!" class="mb-5" />
+    <button @click="toggleDark()">
+      Toggle Color Mode
+    </button>
+    <p>Dark theme: {{ isDark }}</p>
+    <p class="dark:text-red-500 text-green-500">red for dark, green for light</p>
   </header>
 </template>
