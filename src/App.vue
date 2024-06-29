711ea448b4698f8c045fd9add730f7551ee52f3f
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-06-29 23:32:58 +0100

    implement theming

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-import Welcome from '@/components/Welcome.vue';
-import { useDark, useToggle } from '@vueuse/core';
-
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
+import TheWelcome from '@/components/TheWelcome.vue';
+import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
 </script>
 
 <template>
-  <header class="flex flex-col justify-center items-center">
-    <Welcome msg="Hello! Testing!" class="mb-5" />
-    <button @click="toggleDark()">
-      Toggle Color Mode
-    </button>
-    <p>Dark theme: {{ isDark }}</p>
-    <p class="dark:text-red-500 text-green-500">red for dark, green for light</p>
-  </header>
+  <div class="flex flex-col justify-center items-center text-theme-text">
+    <the-welcome msg="Welcome!" class="mb-5" />
+    <theme-switcher />
+  </div>
 </template>
