7aea7f5e68b6c42f3b08d6b4936aa2ca1d722318
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-01 09:12:12 +0100

    format etc

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
+<script setup>
 import { useColorMode } from '@vueuse/core';
+
+const modes = {
+  light: 'light',
+  dark: 'dark',
+  cafe: 'cafe'
+};
 
 const colorMode = useColorMode({
   disableTransition: false,
-  modes: {
-    cafe: 'cafe',
-  },
+  modes,
   attribute: 'theme'
 });
 
@@ -22,13 +26,36 @@ const switchTheme = () => {
 
 <template>
   <div class="flex items-center gap-5">
-    <h1 class="text-xl">Current theme: {{ colorMode }}</h1>
+    <h1 class="text-2xl">
+      Current theme: <span class="text-theme-accent capitalize">{{ colorMode }}</span>
+    </h1>
     <button
       @click="switchTheme"
-      class="p-3 border border-theme-text hover:text-theme-accent hover:border-theme-accent transition-colors"
+      class="p-3 bg-theme-main border border-theme-text hover:text-theme-accent hover:border-theme-accent transition-colors"
     >
       Switch Theme
     </button>
   </div>
-  <p class="text-theme-accent">--accent</p>
+  <div class="text-center mt-12">
+    <h1 class="text-2xl">Theme colours:</h1>
+    <div class="flex">
+      <div class="flex bg-theme-main w-24 h-16 items-center justify-center">main</div>
+      <div class="flex bg-theme-text text-theme-main w-24 h-16 items-center justify-center">
+        text
+      </div>
+      <div class="flex bg-theme-accent w-24 h-16 items-center justify-center">accent</div>
+      <div
+        class="flex bg-theme-bg-primary border border-theme-text w-24 h-16 items-center justify-center"
+      >
+        bg-primary
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-12">
+    <h1 class="text-2xl text-left">Themes:</h1>
+    <ul v-for="mode in modes" :key="mode">
+      <li class="list-disc">{{ mode }}</li>
+    </ul>
+  </div>
 </template>
