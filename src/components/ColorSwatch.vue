ff22d5e4d6a8c5028e1ad98e77dfb9d430ecba96
Mikey Ying <mikeyinguk@gmail.com>
2024-07-08 09:18:21 +0100

    color swatch component

---
+++ src/components/ColorSwatch.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  colorToken: String,
+  borderColorToken: String
+});
+</script>
+
+<template>
+  <div class="flex items-center space-x-2">
+    <div
+      :class="
+        borderColorToken
+          ? `border border-${borderColorToken} w-6 h-6 bg-theme-${colorToken}`
+          : `w-6 h-6 bg-theme-${colorToken}`
+      "
+    ></div>
+    <span class="font-mono">{{ colorToken }}</span>
+    <span class="font-mono">{{ borderColorToken }}</span>
+  </div>
+</template>
