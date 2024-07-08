2bcfe8615a2f15f08bc323f4dae8462a3829eb8a
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-08 09:19:25 +0100

    move styles to app. move message in welcome to slot

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue';
+<script setup>
 import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
+import HomeHero from '@/components/HomeHero.vue';
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center text-theme-text">
-    <the-welcome msg="Welcome!" class="mb-5" />
-    <theme-switcher />
+  <div class="h-screen w-screen text-theme-text grid place-items-center">
+    <home-hero class="grid place-items-center md:grid-cols-2 gap-24 p-8">
+      <template #header>
+        <p class="leading-tight">
+          Hello,
+          <br />
+          I'm Mikey!
+        </p>
+      </template>
+
+      <theme-switcher />
+    </home-hero>
   </div>
 </template>
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1
+      class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-theme-main transition-colors duration-300"
+    >
+      <slot name="header" />
+    </h1>
+
+    <slot name="default" />
+  </div>
+</template>
