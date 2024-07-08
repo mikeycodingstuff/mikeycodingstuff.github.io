07bdd5336b0f1b0e3e528a25f4c47bf75a388f4a
Mikey Ying <mikeyinguk@gmail.com>
2024-07-08 22:55:15 +0100

    dropdown styling

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -21,22 +21,25 @@ const unKebab = (string) => string.replace(/-/g, ' ');
     </p>
 
     <div class="grid space-y-5 w-full">
-      <color-swatch bg-class="bg-theme-main" color-token="main" />
-      <color-swatch bg-class="bg-theme-text" color-token="text" />
-      <color-swatch bg-class="bg-theme-muted" color-token="muted" />
-      <color-swatch bg-class="bg-theme-accent" color-token="accent" />
       <color-swatch
         bg-class="bg-theme-bg-primary"
         border-class="border border-theme-text"
         color-token="bg-primary"
       />
       <color-swatch bg-class="bg-theme-bg-secondary" color-token="bg-secondary" />
+      <color-swatch bg-class="bg-theme-main" color-token="main" />
+      <color-swatch bg-class="bg-theme-accent" color-token="accent" />
+      <color-swatch bg-class="bg-theme-accent-alt" color-token="accent-alt" />
+      <color-swatch bg-class="bg-theme-text" color-token="text" />
       <color-swatch bg-class="bg-theme-error" color-token="error" />
       <color-swatch bg-class="bg-theme-error-alt" color-token="error-alt" />
     </div>
 
     <div>
-      <select v-model="colorMode" class="border border-theme-text bg-transparent">
+      <select
+        v-model="colorMode"
+        class="p-2 rounded bg-theme-main text-theme-bg-primary border-2 border-transparent focus:border-theme-bg-primary focus:outline-none focus:ring-4 focus:ring-theme-main transition ease-in-out duration-300"
+      >
         <option v-for="(theme, key, index) in themes" :value="theme" :key="index">
           {{ theme }}
         </option>
