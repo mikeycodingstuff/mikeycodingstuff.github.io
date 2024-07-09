d67cb4fd4d2f765f94a9c79ef42a913b20955a02
Mikey Ying <mikeyinguk@gmail.com>
2024-07-09 02:13:13 +0100

    wip typewriter component

---
+++ src/components/TypeWriter.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  prompt: String,
+  text: Array,
+});
+</script>
+
+<template>
+  <div class="mt-4">
+    <p class="font-mono text-theme-text">
+      <span class="">{{ prompt }}</span> {{ text[1] }}<span class="inline-block animate-blink">_</span>
+    </p>
+  </div>
+</template>
