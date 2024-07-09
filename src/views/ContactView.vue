9011dbfe5161dbf3124589b0a6b0e25c726e5632
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-09 02:18:19 +0100

    view examples

---
+++ src/views/ContactView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-pink-400">
+    <h1>This is a contact page</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-pink-400">
+    <h1>This is a home page</h1>
+  </div>
+</template>
