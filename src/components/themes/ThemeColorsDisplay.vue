ba2d45ced3f4cbf9ed0bb37fccd11b065a62d7ab
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 16:13:21 +0100

    move theme stuff

---
+++ src/components/themes/ThemeColorsDisplay.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ColorSwatch from '@/components/themes/ColorSwatch.vue';
+</script>
+
+<template>
+  <div class="grid space-y-5">
+    <color-swatch
+      bg-class="bg-theme-bg-primary"
+      border-class="border border-theme-text"
+      color-token="bg-primary"
+    />
+    <color-swatch bg-class="bg-theme-bg-secondary" color-token="bg-secondary" />
+    <color-swatch bg-class="bg-theme-main" color-token="main" />
+    <color-swatch bg-class="bg-theme-accent" color-token="accent" />
+    <color-swatch bg-class="bg-theme-accent-alt" color-token="accent-alt" />
+    <color-swatch bg-class="bg-theme-text" color-token="text" />
+    <color-swatch bg-class="bg-theme-error" color-token="error" />
+    <color-swatch bg-class="bg-theme-error-alt" color-token="error-alt" />
+  </div>
+</template>
---
+++ src/components/themes/ThemeSwitcher.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+import { themes } from '@/themes';
+import { unKebab } from '@/helpers';
+
+const colorMode = useColorMode({
+  attribute: 'theme',
+  disableTransition: false,
+  modes: themes
+});
+</script>
+
+<template>
+  <div class="grid gap-10 place-items-center">
+    <p class="text-2xl text-center text-theme-accent font-mono lowercase transition-colors">
+      {{ unKebab(colorMode) }}
+    </p>
+
+    <div>
+      <select
+        v-model="colorMode"
+        class="p-2 rounded bg-theme-main text-theme-bg-primary border-2 border-transparent focus:border-theme-bg-primary focus:outline-none focus:ring-4 focus:ring-theme-main transition ease-in-out"
+      >
+        <option v-for="(theme, key, index) in themes" :value="theme" :key="index">
+          {{ theme }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
