1f9cc608dca773fbdf2b303925aa691bcd4e375a
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 16:14:25 +0100

    changes to fix nav

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,18 @@
 <script setup lang="ts">
-import HomeHero from '@/components/HomeHero.vue';
-import TypeWriter from '@/components/TypeWriter.vue';
-import HeroNav from '@/components/HeroNav.vue';
 import { useColorMode } from '@vueuse/core';
 import { themes } from '@/themes';
+import HeroNav from '@/components/hero/HeroNav.vue';
 
 useColorMode({
   attribute: 'theme',
   disableTransition: false,
   modes: themes
 });
-
-const typewriterText = ['^_^', 'full stack developer', 'laravel developer'];
 </script>
 
 <template>
-  <div class="h-screen w-screen text-theme-text grid place-items-center">
-    <home-hero class="grid place-items-center md:grid-cols-2 gap-24 p-8">
-      <template #header>
-        Hello,
-        <br />
-        I'm
-        <router-link
-          to="/"
-          class="text-theme-accent hover:text-theme-accent-alt underline transition-colors duration-700 focus:outline-none ease-in-out"
-          >Mikey</router-link
-        >!
-      </template>
-
-      <template #sub>
-        <type-writer prompt=">" :text="typewriterText" />
-      </template>
-      <hero-nav />
-    </home-hero>
+  <div>
+    <hero-nav v-if="$route.path !== '/'"></hero-nav>
     <router-view />
   </div>
 </template>
