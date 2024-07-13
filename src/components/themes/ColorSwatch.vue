48c766af9ee470dd7b97c208f98e563efae5803e
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 22:53:10 +0100

    change prop name

--- src/components/themes/ColorSwatch.vue
+++ src/components/themes/ColorSwatch.vue
@@ -2,13 +2,13 @@
 defineProps({
   bgClass: String,
   borderClass: String,
-  colorToken: String,
+  label: String,
 });
 </script>
 
 <template>
   <div class="flex items-center">
-    <div :class="`w-6 h-6 m-2 ${bgClass} ${borderClass} transition-colors duration-300`"></div>
-    <p class="font-mono lg:text-lg">{{ colorToken }}</p>
+    <div :class="`w-6 h-6 ${bgClass} ${borderClass} transition-colors duration-300`"></div>
+    <p v-if="label" class="lg:text-lg">{{ label }}</p>
   </div>
 </template>
