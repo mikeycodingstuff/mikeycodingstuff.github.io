e4e3d162486ca56b9df42c8c973228c69af215c3
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 22:52:17 +0100

    styling

--- src/components/themes/ThemeSwitcher.vue
+++ src/components/themes/ThemeSwitcher.vue
@@ -11,15 +11,15 @@ const colorMode = useColorMode({
 </script>
 
 <template>
-  <div class="p-12 rounded grid gap-10 place-items-center bg-theme-bg-secondary w-72">
-    <p class="text-2xl text-center text-theme-accent font-mono lowercase transition-colors">
+  <div class="rounded grid gap-10 place-items-center">
+    <p class="text-nowrap text-2xl text-center lowercase">
       {{ unKebab(colorMode) }}
     </p>
 
     <div>
       <select
         v-model="colorMode"
-        class="p-2 rounded bg-theme-main text-theme-bg-primary border-2 border-transparent focus:border-theme-bg-primary focus:outline-none focus:ring-4 focus:ring-theme-main transition ease-in-out"
+        class="p-2 rounded bg-theme-main text-theme-bg border-2 border-transparent focus:border-theme-bg focus:outline-none focus:ring-4 focus:ring-theme-main transition ease-in-out"
       >
         <option v-for="(theme, key, index) in themes" :value="theme" :key="index">
           {{ theme }}
