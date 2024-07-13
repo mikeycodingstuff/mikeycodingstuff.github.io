3e156b892cc116c11ca72ffd1fff68aad1698e12
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 22:53:50 +0100

    wip refactor contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import ExternalLink from '@/components/nav/ExternalLink.vue';
+</script>
+
 <template>
-  <div class="text-center p-12">
-    <h1 class="text-4xl text-theme-main">
-      This is a <span class="text-theme-accent">contact</span> page
-    </h1>
+  <div>
+    <div class="grid place-items-center md:grid-cols-2 gap-12">
+      <div class="text-2xl max-w-2xl">
+        <p>
+          The best way to contact me is via
+          <external-link link="mailto:mikeyinguk+jobs@gmail.com">e-mail</external-link>, however you
+          can also reach me via the links below.
+        </p>
+      </div>
+
+      <div class="text-2xl max-w-2xl">
+        <p>links</p>
+      </div>
+    </div>
   </div>
 </template>
