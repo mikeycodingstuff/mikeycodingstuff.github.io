dcf3e0b9d9211d52c6bdacd90e119753fc2ae07c
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 16:13:21 +0100

    move theme stuff

---
+++ src/components/themes/ColorSwatch.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  bgClass: String,
+  borderClass: String,
+  colorToken: String
+});
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div :class="`w-6 h-6 m-2 ${bgClass} ${borderClass} transition-colors duration-300`"></div>
+    <p class="font-mono lg:text-lg">{{ colorToken }}</p>
+  </div>
+</template>
