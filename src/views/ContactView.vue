9ea07a1a6ca546a33f758b324682e362e18b47ef
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 16:29:43 +0100

    typos in placeholders

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center p-12">
     <h1 class="text-4xl text-theme-main">
-      This is an <span class="text-theme-accent">contact</span> page
+      This is a <span class="text-theme-accent">contact</span> page
     </h1>
   </div>
 </template>
