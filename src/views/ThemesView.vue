26d2fc96bbc05f37d2c1e2adacf0d9739f80ff25
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 17:08:22 +0100

    unnecessary css

--- src/views/ThemesView.vue
+++ src/views/ThemesView.vue
@@ -4,7 +4,7 @@ import ThemeColorsDisplay from '@/components/themes/ThemeColorsDisplay.vue';
 </script>
 
 <template>
-  <div class="flex items-center justify-center p-12 gap-20">
+  <div class="flex justify-center p-12 gap-20">
     <theme-switcher />
     <theme-colors-display />
   </div>
