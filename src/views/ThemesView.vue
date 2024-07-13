ea6fdabc0a62c3a6ba8d73c76cbd5b610e6e8664
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 22:52:03 +0100

    adjust themes view

--- src/views/ThemesView.vue
+++ src/views/ThemesView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import ThemeSwitcher from '@/components/themes/ThemeSwitcher.vue';
 import ThemeColorsDisplay from '@/components/themes/ThemeColorsDisplay.vue';
+import ExternalLink from '@/components/nav/ExternalLink.vue';
 </script>
 
 <template>
-  <div class="flex justify-center p-12 gap-20">
-    <theme-switcher />
-    <theme-colors-display />
+  <div>
+    <div class="grid place-items-center md:grid-cols-2 gap-12">
+      <div class="text-2xl max-w-2xl">
+        <p class="leading-normal text-pretty">
+          Use this theme-switcher, or hit
+          <span class="text-theme-accent-alt">`Esc`</span> to change the theme!
+        </p>
+        <p class="leading-normal">
+          Heavily inspired by
+          <external-link link="https://monkeytype.com">monkeytype</external-link>
+        </p>
+      </div>
+      <div class="grid center p-6 bg-theme-bg-alt rounded-lg">
+        <theme-switcher />
+        <theme-colors-display class="p-4" />
+      </div>
+    </div>
   </div>
 </template>
