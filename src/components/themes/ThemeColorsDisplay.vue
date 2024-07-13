7b4eae88b8e352329568a887bd32cac40f18d11c
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 22:51:47 +0100

    just show color squares for theme display

--- src/components/themes/ThemeColorsDisplay.vue
+++ src/components/themes/ThemeColorsDisplay.vue
@@ -3,18 +3,11 @@ import ColorSwatch from '@/components/themes/ColorSwatch.vue';
 </script>
 
 <template>
-  <div class="grid grid-cols-2 gap-y-5">
-    <color-swatch
-      bg-class="bg-theme-bg-primary"
-      border-class="border border-theme-text"
-      color-token="bg-primary"
-    />
-    <color-swatch bg-class="bg-theme-bg-secondary" color-token="bg-secondary" />
-    <color-swatch bg-class="bg-theme-main" color-token="main" />
-    <color-swatch bg-class="bg-theme-accent" color-token="accent" />
-    <color-swatch bg-class="bg-theme-accent-alt" color-token="accent-alt" />
-    <color-swatch bg-class="bg-theme-text" color-token="text" />
-    <color-swatch bg-class="bg-theme-error" color-token="error" />
-    <color-swatch bg-class="bg-theme-error-alt" color-token="error-alt" />
+  <div class="flex justify-center">
+    <color-swatch bg-class="bg-theme-bg" />
+    <color-swatch bg-class="bg-theme-main" />
+    <color-swatch bg-class="bg-theme-accent" />
+    <color-swatch bg-class="bg-theme-accent-alt" />
+    <color-swatch bg-class="bg-theme-text" />
   </div>
 </template>
