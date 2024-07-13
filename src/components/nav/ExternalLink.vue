5d9f12a836a985c132676da880713ed5f85d3ffd
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-13 22:53:56 +0100

    external link component

---
+++ src/components/nav/ExternalLink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  link: String,
+});
+</script>
+
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    class="rounded text-theme-accent hover:text-theme-accent-alt focus:outline-none focus:ring-4 focus:ring-theme-accent transition duration-300"
+    ><slot
+  /></a>
+</template>
