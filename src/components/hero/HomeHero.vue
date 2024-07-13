98285a4a2680c161146b5b10d5e0e7ddc50924a1
Mikey Ying <mikeyinguk@gmail.com>
2024-07-13 16:13:10 +0100

    move hero stuff

--- src/components/hero/HomeHero.vue
+++ src/components/hero/HomeHero.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <header
-        class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-theme-main transition-colors duration-1000"
+        class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-theme-main transition-colors duration-300"
       >
         <slot name="header" />
       </header>
