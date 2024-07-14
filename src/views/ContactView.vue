90765a11431dae99558efbd91fa624bcbad22ce8
Mikey Ying <mikeyinguk@gmail.com>
2024-07-14 22:48:16 +0100

    update contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
 import ExternalLink from '@/components/nav/ExternalLink.vue';
+import GitHubIcon from '@/components/icons/GitHubIcon.vue';
+import LinkedInIcon from '@/components/icons/LinkedInIcon.vue';
 </script>
 
 <template>
   <div>
-    <div class="grid place-items-center md:grid-cols-2 gap-12">
-      <div class="text-2xl max-w-2xl">
+    <div class="grid place-items-center gap-12">
+      <div class="text-2xl max-w-2xl text-center">
         <p>
           The best way to contact me is via
           <external-link link="mailto:mikeyinguk+jobs@gmail.com">e-mail</external-link>, however you
-          can also reach me via the links below.
+          can also reach me via these links.
         </p>
       </div>
 
-      <div class="text-2xl max-w-2xl">
-        <external-link link="/" contact class="focus:ring-theme-main">
+      <div class="flex gap-x-12 text-2xl">
+        <external-link
+          link="https://github.com/mikeycodingstuff"
+          contact
+          class="focus:ring-theme-main"
+        >
           <div class="group flex gap-3 items-center rounded text-theme-main p-1">
-            <svg
-              viewBox="0 0 24 24"
-              class="h-10 w-10 fill-theme-main group-hover:fill-theme-accent transition-colors duration-300"
-            >
-              <path
-                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-              />
-            </svg>
+            <git-hub-icon class="group-hover:fill-theme-accent" />
             <p class="group-hover:text-theme-accent transition-colors duration-300">GitHub</p>
+          </div>
+        </external-link>
+
+        <external-link
+          link="https://www.linkedin.com/in/mikeyying"
+          contact
+          class="focus:ring-theme-main"
+        >
+          <div class="group flex gap-3 items-center rounded text-theme-main p-1">
+            <linked-in-icon class="group-hover:fill-theme-accent" />
+            <p class="group-hover:text-theme-accent transition-colors duration-300">LinkedIn</p>
           </div>
         </external-link>
       </div>
