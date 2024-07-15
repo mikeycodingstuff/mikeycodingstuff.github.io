e66bd4790d7cd5058fe6b28325e44915ec971998
Mikey Ying <mikeyinguk@gmail.com>
2024-07-15 10:48:22 +0100

    formatting

--- src/components/TypeWriter.vue
+++ src/components/TypeWriter.vue
@@ -9,8 +9,8 @@ defineProps({
   <div>
     <p class="text-2xl">
       <span class="">{{ prompt }}</span>
-      {{ messages[Math.floor(Math.random() * messages.length)] }}
-      <span class="inline-block animate-blink">_</span>
+      {{ messages[Math.floor(Math.random() * messages.length)]
+      }}<span class="inline-block animate-blink">_</span>
     </p>
   </div>
 </template>
