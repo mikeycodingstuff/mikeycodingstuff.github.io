6fc2a50a194cbf04badc5d96c6640595dbbfaeff
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-07-27 13:14:45 +0100

    use temporary modal while no cv

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,5 +1,12 @@
 <script setup>
 import NavLink from '@/components/nav/NavLink.vue';
+import { ref } from 'vue';
+import { vOnClickOutside } from '@vueuse/components';
+
+const modal = ref(false);
+function closeModal() {
+  modal.value = false;
+}
 </script>
 
 <template>
@@ -9,10 +16,41 @@ import NavLink from '@/components/nav/NavLink.vue';
     <nav-link link="contact" name="contact" />
     <nav-link link="projects" name="projects" />
     <nav-link link="themes" name="themes" />
-    <button
-      class="rounded p-1 px-4 bg-theme-main text-theme-bg hover:bg-theme-text focus:outline-none focus:ring-4 focus:ring-theme-text underline-offset-4 transition duration-300"
-    >
-      download cv
-    </button>
+    <!--    <a href="/mikey-ying-cv.pdf" download="mikey-ying-cv.pdf" class="inline-block">-->
+    <!--      <button-->
+    <!--        class="rounded w-full h-full p-1 px-4 text-nowrap bg-theme-main text-theme-bg hover:bg-theme-text focus:outline-none focus:ring-4 focus:ring-theme-text underline-offset-4 transition duration-300"-->
+    <!--      >-->
+    <!--        download cv-->
+    <!--      </button>-->
+    <!--    </a>-->
+    <div class="relative">
+      <button
+        @click="modal = true"
+        class="rounded p-1 px-4 w-full h-full text-nowrap bg-theme-main text-theme-bg hover:bg-theme-text focus:outline-none focus:ring-4 focus:ring-theme-text underline-offset-4 transition duration-300"
+      >
+        download cv
+      </button>
+      <transition name="fade">
+        <div
+          v-if="modal"
+          v-on-click-outside="closeModal"
+          class="absolute left-1/2 transform -translate-x-1/2 rounded p-4 mt-4 bg-theme-main text-theme-bg selection:bg-theme-text w-max items-center"
+        >
+          Todo: rewrite CV 😅
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 300ms ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
