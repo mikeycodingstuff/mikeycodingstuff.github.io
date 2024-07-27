ac5e1ba1ceab4047bbf17a9b87e955738442a02e
Mikey Ying <mikeyinguk@gmail.com>
2024-07-27 13:14:08 +0100

    rename prop for clarity

--- src/components/nav/ExternalLink.vue
+++ src/components/nav/ExternalLink.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 defineProps({
   link: String,
-  contact: Boolean,
+  isContactLink: Boolean,
 });
 </script>
 
@@ -11,7 +11,7 @@ defineProps({
     target="_blank"
     :class="[
       'rounded text-theme-accent hover:text-theme-accent-alt focus:outline-none focus:ring-4 focus:ring-theme-accent transition duration-300',
-      contact ? 'flex gap-3 items-center' : '',
+      isContactLink ? 'flex gap-3 items-center' : '',
     ]"
     ><slot
   /></a>
