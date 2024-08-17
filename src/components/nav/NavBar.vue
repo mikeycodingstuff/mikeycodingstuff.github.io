f8867421f9489ea4c3283719d3bc41616f88788e
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-08-17 12:20:47 +0100

    add todo

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -10,6 +10,7 @@ function closeModal() {
 }
 </script>
 
+<!--TODO: responsiveness on mobile. probably hamburger menu-->
 <template>
   <div class="flex flex-col md:flex-row justify-center md:items-center my-3 flex-wrap">
     <nav-link link="/" name="home" />
