f9414103d09e438bca002d95af7505014ac889d7
Mikey Ying <mikeyinguk@gmail.com>
2024-08-17 09:32:29 +0100

    styling/responsiveness

--- src/components/hero/HomeHero.vue
+++ src/components/hero/HomeHero.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <div>
-      <header
-        class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-theme-main transition-colors duration-300"
-      >
-        <slot name="header" />
-      </header>
+    <div class="p-10">
+      <div class="flex justify-center">
+        <header
+          class="text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-theme-main transition-colors duration-300"
+        >
+          <slot name="header" />
+        </header>
+      </div>
       <slot name="sub" />
     </div>
 
