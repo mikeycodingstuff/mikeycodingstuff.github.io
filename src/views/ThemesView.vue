129200255501225fbac080ab15b72e063a47e29c
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-08-17 12:24:02 +0100

    comment out wip code. add todos

--- src/views/ThemesView.vue
+++ src/views/ThemesView.vue
@@ -7,11 +7,15 @@ import ExternalLink from '@/components/nav/ExternalLink.vue';
 <template>
   <div>
     <div class="flex flex-col md:flex-row items-center p-3 gap-y-10">
-      <div class="text-2xl max-w-2xl">
+      <div class="text-2xl max-w-2xl p-3">
         <p class="leading-normal text-pretty">
-          Use this theme-switcher, or hit
-          <span class="text-theme-accent-alt">`Esc`</span> to change the theme!
+          Use this theme-switcher to change the theme!
         </p>
+<!--        TODO: implement command bar-->
+<!--        <p class="leading-normal text-pretty">-->
+<!--          Use this theme-switcher, or hit-->
+<!--          <span class="text-theme-accent-alt">`Esc`</span> to change the theme!-->
+<!--        </p>-->
         <br />
         <p class="leading-normal">
           Heavily inspired by
