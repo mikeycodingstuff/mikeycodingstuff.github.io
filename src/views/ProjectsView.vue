fcb3f9acb63c426d1eea550eb5e2fd95479f3e43
Mikey Ying <mikeyinguk@gmail.com>
2024-08-17 12:13:15 +0100

    change temp message. consistency in script setup

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import ExternalLink from '@/components/nav/ExternalLink.vue';
+</script>
+
 <template>
   <div class="text-center p-12">
     <h1 class="text-4xl text-theme-main">
-      TODO: <span class="text-theme-accent">projects page</span>
+      TODO: <span class="text-theme-accent">projects page</span>!
     </h1>
+    <p class="mt-6 text-2xl">For now, feel free to check out my <external-link link="https://github.com/mikeycodingstuff">GitHub</external-link>!</p>
   </div>
 </template>
