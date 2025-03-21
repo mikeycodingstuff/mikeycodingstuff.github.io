49e5d31e084f34088c50c54d14c328f2d0d2f758
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-03-21 21:37:39 +0000

    dont show select until loaded

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { unKebab } from '@/helpers'
 import { themes } from '@/themes'
+
+const colorMode = useColorMode()
 </script>
 
 <template>
@@ -16,13 +18,13 @@ import { themes } from '@/themes'
       </template>
 
       <p class="text-nowrap text-2xl text-center lowercase">
-        {{ unKebab($colorMode.preference) }}
+        {{ unKebab(colorMode.preference) }}
       </p>
     </ColorScheme>
 
-    <div>
+    <div v-if="!colorMode.unknown">
       <select
-        v-model="$colorMode.preference"
+        v-model="colorMode.preference"
         class="p-2 rounded-sm bg-theme-main text-theme-bg border-2 border-transparent focus:border-theme-bg focus:outline-hidden focus:ring-4 focus:ring-theme-main transition ease-in-out"
       >
         <option v-for="(theme, key, index) in themes" :key="index" :value="theme">
