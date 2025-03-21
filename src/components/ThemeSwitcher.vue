8d11a5861983aee94fe31b253902f40cc1cf5473
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-03-21 21:24:02 +0000

    simple loading spinner

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -5,7 +5,16 @@ import { themes } from '@/themes'
 
 <template>
   <div class="rounded-sm grid gap-10 place-items-center">
-    <ColorScheme placeholder="...loading" class="text-nowrap text-2xl text-center lowercase">
+    <ColorScheme class="text-nowrap text-2xl text-center lowercase">
+      <template #placeholder>
+        <div class="flex justify-center items-center">
+          <svg class="animate-spin h-6 w-6 text-theme-main" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4l4-4-4-4v4a8 8 0 00-8 8h4z" />
+          </svg>
+        </div>
+      </template>
+
       <p class="text-nowrap text-2xl text-center lowercase">
         {{ unKebab($colorMode.preference) }}
       </p>
