7b09d182342bb2e0b9b1d865ccbd8ac60c8da8c9
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-03-22 16:18:26 +0000

    refactor hamburger animations

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,18 +5,21 @@ function toggleMenu() {
   open.value = !open.value
 }
 
-const isHomePage = computed(() => useRoute().path === '/')
+const route = useRoute()
+const isHomePage = computed(() => route.path === '/')
 
-watch(() => useRoute().path, () => {
-  open.value = false
-})
+watch(() => route.path, () => open.value = false)
 </script>
 
 <template>
   <div class="flex md:justify-center items-center">
     <!-- Hamburger Menu Button -->
-    <!-- TODO: add animations - refactor to alpine? -->
-    <button v-if="!isHomePage" class="p-2 focus:outline-hidden md:hidden" @click="toggleMenu">
+    <!-- TODO: refactor to alpine? -->
+    <button
+      v-if="!isHomePage"
+      class="p-2 focus:outline-hidden md:hidden"
+      @click="toggleMenu"
+    >
       <svg
         class="w-12 h-12 text-theme-main"
         xmlns="http://www.w3.org/2000/svg"
@@ -34,14 +37,14 @@ watch(() => useRoute().path, () => {
     </button>
   </div>
 
-  <div>
-    <!--        TODO: implement command bar -->
-    <!--        <div class="p-2 hidden md:block"> -->
-    <!--          <p class="text-center md:text-sm lg:text-base xl:text-lg transition-colors duration-300"> -->
-    <!--            Press <span class="text-theme-accent-alt">`Esc`</span> to navigate via the command bar -->
-    <!--          </p> -->
-    <!--        </div> -->
-  </div>
+  <!--        TODO: implement command bar -->
+  <!--  <div> -->
+  <!--        <div class="p-2 hidden md:block"> -->
+  <!--          <p class="text-center md:text-sm lg:text-base xl:text-lg transition-colors duration-300"> -->
+  <!--            Press <span class="text-theme-accent-alt">`Esc`</span> to navigate via the command bar -->
+  <!--          </p> -->
+  <!--        </div> -->
+  <!--  </div> -->
 
   <!-- Desktop Navigation -->
   <div
@@ -66,14 +69,28 @@ watch(() => useRoute().path, () => {
   </div>
 
   <!-- Mobile Navigation -->
-  <div
-    v-if="open"
-    class="top-0 flex flex-col lg:flex-row justify-center lg:items-center  flex-wrap"
-  >
-    <NavLink to="/" name="home" />
-    <NavLink to="/about" name="about" />
-    <NavLink to="/projects" name="projects" />
-    <NavLink to="/themes" name="themes" />
-    <hr class="w-48 mb-8 mt-4 h-1 mx-auto bg-theme-main rounded-md">
-  </div>
+  <Transition :name="open ? 'slide-fade' : ''">
+    <div
+      v-if="open"
+      class="top-0 flex flex-col lg:flex-row justify-center lg:items-center flex-wrap"
+    >
+      <NavLink to="/" name="home" />
+      <NavLink to="/about" name="about" />
+      <NavLink to="/projects" name="projects" />
+      <NavLink to="/themes" name="themes" />
+      <hr class="w-48 mb-8 mt-4 h-1 mx-auto bg-theme-main rounded-md">
+    </div>
+  </Transition>
 </template>
+
+<style>
+/* Slide-Fade Animation */
+.slide-fade-enter-active {
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+.slide-fade-enter-from {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+</style>
